56522d7c92fb2a700d69d509766aca7fa6270977
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-05 18:34:59 -0600

    App.js

--- src/App.js
+++ src/App.js
@@ -35,10 +35,10 @@ function App(props) {
               </div>
             </div>
             <div className="row">
-              <Comments newComments={props.newComments} />
-              <Tasks newTasks={props.newTasks} />
-              <Orders newOrders={props.newOrders} />
-              <Tickets tickets={props.tickets} />
+              <Comments  />
+              <Tasks  />
+              <Orders />
+              <Tickets />
             </div>
             <AreaChart />
             <div className="row">
