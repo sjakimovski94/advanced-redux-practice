8dad837b12301fb16d9f86487a017e34b2768223
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-05 18:31:56 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import state from "./state";
+import store from "./store";
+import {Provider} from 'react-redux';
+
+
 
 const {
 dateTime,
-newComments,
-newTasks,
-newOrders,
-tickets,
 orders,
 taskItem,
 tasks,
@@ -17,16 +17,14 @@ messages
 } = state;
 
 ReactDOM.render(
+  <Provider store={store}>
   <App 
     taskItem={taskItem}
     dateTime={dateTime}
-    newComments={newComments}
-    newTasks={newTasks}
-    newOrders={newOrders}
-    tickets={tickets}
     orders={orders} 
     messages={messages}
     tasks={tasks}
-    />,
+    />
+    </Provider>,
   document.getElementById("root")
 );
